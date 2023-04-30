7815c2188607c4d7c47674e0a3715bfa3263affd
Salah JAAFAR <47123877+salahjaafar@users.noreply.github.com>
2023-04-30 23:05:19 +0100

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,94 +1,38 @@
 <?php
-/**
- * @package     Persada.Site
- * @subpackage  mod_slider
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die;
 
-$doc = JFactory::getDocument();
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 
-$cssSizeInfo = getimagesize($params->get('slide1Img'));
-$doc->addStyleDeclaration("
-#home-slider {
-height: $cssSizeInfo[1]px;
+// Get the module parameters
+$params = JFactory::getApplication()->input->get('params');
+
+// Get the number of slides
+$count = (int) $params->get('count');
+
+// Get the slides
+$slides = [];
+for ($i = 1; $i <= $count; $i++)
+{
+  $slides[] = [
+    'img' => $params->get('slide' . $i . 'Img'),
+    'title' => $params->get('slide' . $i . 'CaptionTitle'),
+    'text' => $params->get('slide' . $i . 'CaptionText'),
+  ];
 }
-");
 
 ?>
 
 <div class="flexslider">
-	<ul class="slides">
-		<?php if ($params->get('slide1Img')) : ?>
-		<li>
-			<img src="<?php echo $params->get('slide1Img'); ?>" alt="">
-			<div class="caption-wrapper">
-				<?php if ($params->get('slide1CaptionTitle') || $params->get('slide1CaptionText')) : ?>
-				<div class="caption" data-right="5%">
-					<?php if ($params->get('slide1CaptionTitle')) : ?>
-					<h2 class="slide-title"><?php echo $params->get('slide1CaptionTitle'); ?></h2>
-					<?php endif; ?>
-					<?php if ($params->get('slide1CaptionText')) : ?>
-					<p class="slide-text"><?php echo $params->get('slide1CaptionText'); ?></p>
-					<?php endif; ?>
-				</div>
-				<?php endif; ?>
-			</div>
-		</li>
-		<?php endif; ?>
-		<?php if ($params->get('slide2Img')) : ?>
-			<li>
-				<img src="<?php echo $params->get('slide2Img'); ?>" alt="">
-				<div class="caption-wrapper">
-					<?php if ($params->get('slide2CaptionTitle') || $params->get('slide2CaptionText')) : ?>
-						<div class="caption" data-right="5%">
-							<?php if ($params->get('slide2CaptionTitle')) : ?>
-								<h2 class="slide-title"><?php echo $params->get('slide2CaptionTitle'); ?></h2>
-							<?php endif; ?>
-							<?php if ($params->get('slide2CaptionText')) : ?>
-								<p class="slide-text"><?php echo $params->get('slide2CaptionText'); ?></p>
-							<?php endif; ?>
-						</div>
-					<?php endif; ?>
-				</div>
-			</li>
-		<?php endif; ?>
-		<?php if ($params->get('slide3Img')) : ?>
-			<li>
-				<img src="<?php echo $params->get('slide3Img'); ?>" alt="">
-				<div class="caption-wrapper">
-					<?php if ($params->get('slide3CaptionTitle') || $params->get('slide3CaptionText')) : ?>
-						<div class="caption" data-right="5%">
-							<?php if ($params->get('slide3CaptionTitle')) : ?>
-								<h2 class="slide-title"><?php echo $params->get('slide3CaptionTitle'); ?></h2>
-							<?php endif; ?>
-							<?php if ($params->get('slide3CaptionText')) : ?>
-								<p class="slide-text"><?php echo $params->get('slide3CaptionText'); ?></p>
-							<?php endif; ?>
-						</div>
-					<?php endif; ?>
-				</div>
-			</li>
-		<?php endif; ?>
-		<?php if ($params->get('slide4Img')) : ?>
-			<li>
-				<img src="<?php echo $params->get('slide4Img'); ?>" alt="">
-				<div class="caption-wrapper">
-					<?php if ($params->get('slide4CaptionTitle') || $params->get('slide4CaptionText')) : ?>
-						<div class="caption" data-right="5%">
-							<?php if ($params->get('slide4CaptionTitle')) : ?>
-								<h2 class="slide-title"><?php echo $params->get('slide4CaptionTitle'); ?></h2>
-							<?php endif; ?>
-							<?php if ($params->get('slide4CaptionText')) : ?>
-								<p class="slide-text"><?php echo $params->get('slide4CaptionText'); ?></p>
-							<?php endif; ?>
-						</div>
-					<?php endif; ?>
-				</div>
-			</li>
-		<?php endif; ?>
-	</ul>
+  <ul class="slides">
+    <?php foreach ($slides as $slide) : ?>
+      <li>
+        <img src="<?php echo $slide['img']; ?>" alt="">
+        <div class="caption-wrapper">
+          <h2 class="slide-title"><?php echo $slide['title']; ?></h2>
+          <p class="slide-text"><?php echo $slide['text']; ?></p>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
 </div>
